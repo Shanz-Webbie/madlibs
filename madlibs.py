a9ca9dd27f7ff4cd7fb4ac0c44067a16ad9c1001
Shannon Webster <shanoonwebbie@gmail.com>
2024-05-09 20:57:08 -0700

    second day of CSS lab

--- madlibs.py
+++ madlibs.py
@@ -25,6 +25,29 @@ AWESOMENESS = [
     "lovely",
 ]
 
+NOUNS = [
+"Apple",
+"Car",
+"Tree",
+"Book",
+"Dog",
+"Cat",
+"House",
+"Chair",
+"Sun",
+"Moon",
+"Ocean",
+"Mountain",
+"River",
+"Flower",
+"Pen",
+"Table",
+"Laptop",
+"Bird",
+"Fish",
+"Bicycle",
+]
+
 
 @app.route("/")
 def start_here():
@@ -48,7 +71,7 @@ def greet_person():
     """Greet user and ask if they want to play MadLibs."""
 
     player = request.args.get("person")
-    return render_template("compliment.html", person= player)
+    return render_template("madlibs.html", person= player)
 
 @app.route("/game")
 def show_madlib_form():
@@ -57,7 +80,19 @@ def show_madlib_form():
     if wants_to_play_a_game == "no":
         return render_template("goodbye.html")
     elif wants_to_play_a_game == "yes":
-        return render_template("compliment.html", person=player)
+        return render_template("game.html", person=player)
+    
+@app.route("/madlib")
+def show_madlib():
+    color = request.args.get("color")
+    # noun1 = request.form.get("noun1")
+    # noun2 = request.form.get("noun2")
+    # noun3 = request.form.get("noun3")
+    adjective = request.args.get("adjective")
+    person = request.args.get("person")
+    print(f"There once was a {{ color }} {{ noun1 }} sitting in the Hackbright Lab. When {{ person }} went to pick it up, it burst into flames in a totally {{ adjective }} way.")
+
+    return render_template("show_madlib.html", color=color, adjective=adjective, person=person)
 
    
 
